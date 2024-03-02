1a702b316d7c314721e62c0576c2a8387d62a4f3
TAHA SHEIKH <tahasheikh111.ts@gmail.com>
2024-03-02 23:46:57 +0500

    day1

---
+++ merge_sort_array.cpp
@@ -0,0 +1,54 @@
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+*/
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    Solution() {}
+    ~Solution() {}
+
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        while (m > 0 && n > 0)
+        {
+            if (nums1[m - 1] < nums2[n - 1])
+            {
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
+            }
+            else
+            {
+                nums1[m + n - 1] = nums1[m - 1];
+                m--;
+            }
+        }
+        while(n>0){
+            nums1[m+n-1]=nums2[n-1];
+            n--;
+        }
+    }
+};
+int main()
+{
+    vector<int> nums1 = {1, 2, 3, 0, 0, 0};
+    int m = 3;
+    vector<int> nums2 = {2, 5, 6};
+    int n = 3;
+    Solution s1;
+    s1.merge(nums1, m, nums2, n);
+    for (auto i : nums1)
+    {
+        cout << i << endl;
+    }
+}
